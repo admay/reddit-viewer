9915759201d7cf774eda61aa8dddaed0988117fa
Michael Zavarella <michael.zavarella@capitalone.com>
2017-08-17 13:09:10 -0400

    First commit

---
+++ src/reddit_viewer/core.cljs
@@ -0,0 +1,89 @@
+(ns reddit-viewer.core
+    (:require
+     [ajax.core :as ajax]
+     [reagent.core :as r]
+     [reddit-viewer.chart :as chart]))
+
+(defonce posts (r/atom nil))
+
+(defn find-posts-with-preview [posts]
+  (filter #(= (:post_hint %) "image") posts))
+
+(defn load-posts
+  ([] (load-posts "cat"))
+  ([sub]
+   (ajax/GET (str "http://www.reddit.com/r/" sub ".json?sort=hot&limit=500")
+             {:handler         #(->> (get-in % [:data :children])
+                                     (map :data)
+                                     (find-posts-with-preview)
+                                     (take 12)
+                                     (reset! posts))
+              :response-format :json
+              :keywords?       true})))
+
+;; -------------------------
+;; Views
+
+(defn display-post [{:keys [permalink subreddit title score url]}]
+  [:div.card.m-2
+   [:div.card-block
+    [:h4.card-title
+     [:a {:href (str "http://reddit.com" permalink)} title " "]]
+    [:div [:span.badge.badge-info {:color "info"} subreddit " score " score]]
+    [:img {:width "300px" :src url}]]])
+
+(defn display-posts [posts]
+  (when-not (empty? posts)
+    [:div
+     (for [posts-row (partition-all 3 posts)]
+       ^{:key posts-row}
+       [:div.row
+        (for [post posts-row]
+          ^{:key post}
+          [:div.col-4 [display-post post]])])]))
+
+(defn sort-posts [title sort-key]
+  (when-not (empty? @posts)
+    [:button.btn.btn-secondary
+     {:on-click #(swap! posts (partial sort-by sort-key))}
+     (str "sort posts by " title)]))
+
+(defn navitem [title view id]
+  [:li.nav-item
+   {:class-name (when (= id @view) "active")}
+   [:a.nav-link
+    {:href "#"
+     :on-click #(reset! view id)}
+    title]])
+
+(defn navbar [view]
+  [:nav.navbar.navbar-toggleable-md.navbar-light.bg-faded
+   [:ul.navbar-nav.mr-auto.nav
+    {:className "navbar-nav mr-auto"}
+    [navitem "Posts" view :posts]
+    [navitem "Chart" view :chart]]])
+
+(defn home-page []
+  (r/with-let [view (r/atom :chart)]
+      [:div
+       [navbar view]
+       [:div.card>div.card-block
+        [:div.btn-group
+         [sort-posts "score" :score]
+         [sort-posts "comments" :num_comments]]
+        (case @view
+          :chart [chart/chart-posts-by-votes posts]
+          :posts [display-posts @posts])]]))
+
+;; -------------------------
+;; Initialize app
+
+#_(load-posts "pictures")
+#_(count @posts)
+#_(reset! posts nil)
+
+(defn mount-root []
+  (r/render [home-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (mount-root))
