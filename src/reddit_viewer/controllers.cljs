b02a14ae0c1715d0ecb1d404034ceca18d385de3
Michael Zavarella <michael.zavarella@capitalone.com>
2017-08-18 15:16:45 -0400

    fully re-framed, added subreddit picking input

--- src/reddit_viewer/controllers.cljs
+++ src/reddit_viewer/controllers.cljs
@@ -1,62 +1,64 @@
 (ns reddit-viewer.controllers
   (:require
-   [ajax.core :as ajax]
-   [re-frame.core :as rf]))
+    [ajax.core :as ajax]
+    [re-frame.core :as rf]))
 
 (rf/reg-event-db
- :initialize-db
- (fn [_ _]
-   {:view :posts
-    :sort-key :score
-    :sub "pictures"}))
+  :initialize-db
+  (fn [_ _]
+    {:view     :posts
+     :sort-key :score
+     :sub "pictures"}))
 
 (defn find-posts-with-preview [posts]
-  (filter #(= "image" (:post_hint %)) posts))
+  (filter #(= (:post_hint %) "image") posts))
 
 (rf/reg-event-db
- :set-posts
- (fn [db [_ posts]]
-   (assoc db :posts
-          (->> (get-in posts [:data :children])
-               (map :data)
-               (find-posts-with-preview)))))
+  :set-posts
+  (fn [db [_ posts]]
+    (assoc db :posts
+              (->> (get-in posts [:data :children])
+                   (map :data)
+                   (find-posts-with-preview)))))
 
 (rf/reg-event-db
- :load-posts
- (fn [db [_ sub]]
-   (ajax/GET (str "http://reddit.com/r/" sub ".json?sort=new&limit=50")
-             {:handler #(rf/dispatch [:set-posts %])
-              :response-format :json
-              :keywords? true})
-   db))
+  :load-posts
+  (fn [db [_ sub]]
+    (ajax/GET (str "http://www.reddit.com/r/" sub ".json?count=500")
+              {:handler         #(rf/dispatch [:set-posts %])
+               :error-handler   #(.log js/console (str "The sub: " sub " could not be reached"))
+               :response-format :json
+               :keywords?       true})
+    (assoc db :sub sub)))
 
 (rf/reg-event-db
- :sort-posts
- (fn [db [_ sort-key]]
-   (update db :posts (partial sort-by sort-key >))))
+  :sort-posts
+  (fn [db [_ sort-key]]
+    (update db :posts (partial sort-by sort-key >))))
 
 (rf/reg-event-db
- :select-view
- (fn [db [_ view]]
-   (assoc db :view view)))
+  :select-view
+  (fn [db [_ view]]
+    (assoc db :view view)))
 
 (rf/reg-event-db
  :select-sub
  (fn [db [_ sub]]
-   (assoc db :sub sub)))
+   (do
+     (rf/dispatch [:load-posts sub])
+     (assoc db :sub sub))))
 
 (rf/reg-sub
- :view
- (fn [db _]
-   (:view db)))
+  :view
+  (fn [db _]
+    (:view db)))
+
+(rf/reg-sub
+  :posts
+  (fn [db _]
+    (:posts db)))
 
 (rf/reg-sub
  :sub
  (fn [db _]
    (:sub db)))
-
-(rf/reg-sub
- :posts
- (fn [db _]
-   (:posts db)))
-
--- src/reddit_viewer/core.cljs
+++ src/reddit_viewer/core.cljs
@@ -1,23 +1,58 @@
 (ns reddit-viewer.core
-    (:require
-     [ajax.core :as ajax]
-     [reagent.core :as r]
-     [reddit-viewer.chart :as chart]
-     [reddit-viewer.controllers]
-     [re-frame.core :as rf]))
+  (:require
+    [reagent.core :as r]
+    [reddit-viewer.chart :as chart]
+    [reddit-viewer.controllers]
+    [re-frame.core :as rf]))
 
 ;; -------------------------
 ;; Views
+
+(defn display-post [{:keys [permalink num_comments subreddit title score url]}]
+  [:div.card.m-2
+   [:div.card-block
+    [:h4.card-title
+     [:a {:href (str "http://reddit.com" permalink)} title " "]]
+    [:div [:span.badge.badge-info
+           {:color "info"}
+           subreddit " score " score " / comments " num_comments]]
+    [:img {:width "300px" :src url}]]])
+
+(defn display-posts [posts]
+  (when-not (empty? posts)
+    [:div
+     (for [posts-row (partition-all 3 posts)]
+       ^{:key posts-row}
+       [:div.row
+        (for [post posts-row]
+          ^{:key post}
+          [:div.col-4 [display-post post]])])]))
 
 (defn sort-posts [title sort-key]
   [:button.btn.btn-secondary
    {:on-click #(rf/dispatch [:sort-posts sort-key])}
    (str "sort posts by " title)])
 
-(defn pick-sub []
-  [:input {:type "text"
-           :value sub
-           :on-change #(rf/dispatch [:select-sub sub])}])
+(defn navitem [title view id]
+  [:li.nav-item
+   {:class-name (when (= id view) "active")}
+   [:a.nav-link
+    {:href     "#"
+     :on-click #(rf/dispatch [:select-view id])}
+    title]])
+
+(defn sub-input [sub]
+  [:input {:type :text
+           :value @(rf/subscribe [:sub])
+           :on-change #(rf/dispatch [:select-sub (-> % .-target .-value)])}])
+
+(defn navbar [view sub]
+  [:nav.navbar.navbar-toggleable-md.navbar-light.bg-faded
+   [:ul.navbar-nav.mr-auto.nav
+    {:className "navbar-nav mr-auto"}
+    [navitem "Posts" view :posts]
+    [navitem "Chart" view :chart]
+    [sub-input sub]]])
 
 (defn home-page []
   (let [view @(rf/subscribe [:view])
@@ -26,21 +61,11 @@
      [navbar view]
      [:div.card>div.card-block
       [:div.btn-group
-       [sort-posts "score" score]
+       [sort-posts "score" :score]
        [sort-posts "comments" :num_comments]]
       (case view
         :chart [chart/chart-posts-by-votes]
         :posts [display-posts @(rf/subscribe [:posts])])]]))
-
-
-
-
-
-
-
-
-
-
 
 ;; -------------------------
 ;; Initialize app
@@ -49,6 +74,6 @@
   (r/render [home-page] (.getElementById js/document "app")))
 
 (defn init! []
-  (rf/dispatch-sync [:intialize-db])
-  (rf/dispatch [:load-posts "pictures"])
+  (rf/dispatch-sync [:initialize-db])
+  (rf/dispatch [:load-posts "telephotolandscapes"])
   (mount-root))
